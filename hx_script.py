44c9f8833157c406fead41d0fb2c59c04f97bd5b
Ytnxne571 <123819213+Ytnxne571@users.noreply.github.com>
2023-04-18 23:22:44 +0800

    Update hx_script.py

--- hx_script.py
+++ hx_script.py
@@ -3,33 +3,44 @@ from datetime import datetime
 from notion_client import Client
 from notion.block import TextBlock
 
-# Notion API key
+# Notion API key，需要修改为自己的 API key
 NOTION_API_KEY = "secret_VMxqT0pKsZMmziMrDD9FOR87uFz1dsMSngOOlGZ4UAT"
 
-# Notion 数据库名称
+# Notion 数据库名称，需要修改为自己的数据库名称
 NOTION_DATABASE_NAME = "My Book Database"
+
+# Notion 父页面 ID，需要修改为自己的页面 ID
+NOTION_PARENT_PAGE_ID = "7a5161c68a784b2a9f3bf5ae54998d6b"
+
+# 书籍信息，需要修改为自己的书籍信息
+book_name = "The Great Gatsby"
+book_cover = "https://images-na.ssl-images-amazon.com/images/I/51TtT1wK0jL._SX331_BO1,204,203,200_.jpg"
+book_author = "F. Scott Fitzgerald"
+book_publisher = "Scribner"
+book_pubdate = datetime(1925, 4, 10).strftime('%Y-%m-%d')
+progress_percent = 50
 
 # 初始化 Notion 客户端
 notion = Client(auth=NOTION_API_KEY)
 
 # 检查是否已经存在名为 NOTION_DATABASE_NAME 的数据库
-results = notion.search(query=NOTION_DATABASE_NAME, filter={"property": "object", "value": "database"}).get("results")
-if len(results) == 0:
+result = notion.search(query=NOTION_DATABASE_NAME, filter={"property": "object", "value": "database"}).get("results")
+if len(result) == 0:
     # 如果不存在，则创建一个新的数据库
-    database = notion.databases.create(parent={"page_id": NotionParentPageID}, title=[{"type": "text", "text": {"content": NOTION_DATABASE_NAME}}])
+    new_database = {
+        "Name": {
+            "title": [{"text": {"content": NOTION_DATABASE_NAME}}]
+        },
+        "Description": {
+            "rich_text": [{"text": {"content": "This is my book database."}}]
+        }
+    }
+    database = notion.databases.create(parent={"page_id": NOTION_PARENT_PAGE_ID}, properties=new_database)
     print(f"Created database \"{NOTION_DATABASE_NAME}\" with ID \"{database.id}\"")
 else:
     # 如果已经存在，则使用该数据库
-    database = notion.databases.retrieve(database_id=results[0].id)
+    database = notion.databases.retrieve(database_id=result[0].id)
     print(f"Retrieved database \"{NOTION_DATABASE_NAME}\" with ID \"{database.id}\"")
-
-# 获取书籍信息
-book_name = ""
-book_cover = ""
-book_author = ""
-book_publisher = ""
-book_pubdate = ""
-progress_percent = 0
 
 # 在 Notion 中检查这本书是否已经存在
 existing_page = None
